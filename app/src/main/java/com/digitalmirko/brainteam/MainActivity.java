3dca9270f2f64a693c3e5b9b8aa3ffe847ce2e9d
Mirko Cukich <mirkocukich@gmail.com>
2016-12-01 16:29:43 -0600

    Added ListView with brain team names

--- app/src/main/java/com/digitalmirko/brainteam/MainActivity.java
+++ app/src/main/java/com/digitalmirko/brainteam/MainActivity.java
@@ -2,12 +2,28 @@ package com.digitalmirko.brainteam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] brainTeam = new String[]{"Nikola Tesla","Elon Musk","Bill Gates","Steve Jobs", "Larry Page", "Sergey Brin"};
+
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> brainTeamAdapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_list_item_1, brainTeam);
+
+        listView.setAdapter(brainTeamAdapter);
+
     }
 }
