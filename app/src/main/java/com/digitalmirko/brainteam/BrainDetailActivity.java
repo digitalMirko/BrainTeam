ecdc5c008d83aa71071914be297d7a9a200be804
Mirko Cukich <mirkocukich@gmail.com>
2016-12-01 17:38:06 -0600

    ListView Taps verified

--- app/src/main/java/com/digitalmirko/brainteam/BrainDetailActivity.java
+++ app/src/main/java/com/digitalmirko/brainteam/BrainDetailActivity.java
@@ -2,17 +2,29 @@ package com.digitalmirko.brainteam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BrainDetailActivity extends AppCompatActivity {
+
+    TextView nameTextView;
+    TextView bornTextView;
+    TextView professionTextView;
+    TextView detailsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brain_detail);
 
-        String personName = (String) getIntent().getExtras().get("personName");
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        bornTextView = (TextView) findViewById(R.id.bornTextView);
+        professionTextView = (TextView) findViewById(R.id.professionTextView);
+        detailsTextView = (TextView) findViewById(R.id.detailsTextView);
 
-        System.out.println("The person is here! " + personName); // Test to see if click works
+        String personName = (String) getIntent().getExtras().get("personName");
+//        System.out.println("The person is here! " + personName); // Test to see if click works
+
+        nameTextView.setText(personName);
 
     }
 
