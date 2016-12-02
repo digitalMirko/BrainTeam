6a04678ca8b5a22bd5a899d123d2417dfc24e3f4
Mirko Cukich <mirkocukich@gmail.com>
2016-12-02 15:51:02 -0600

    Added Additional Photos

--- app/src/main/java/com/digitalmirko/brainteam/BrainDetailActivity.java
+++ app/src/main/java/com/digitalmirko/brainteam/BrainDetailActivity.java
@@ -1,7 +1,9 @@
 package com.digitalmirko.brainteam;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BrainDetailActivity extends AppCompatActivity {
@@ -10,6 +12,7 @@ public class BrainDetailActivity extends AppCompatActivity {
     TextView bornTextView;
     TextView professionTextView;
     TextView detailsTextView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class BrainDetailActivity extends AppCompatActivity {
         bornTextView = (TextView) findViewById(R.id.bornTextView);
         professionTextView = (TextView) findViewById(R.id.professionTextView);
         detailsTextView = (TextView) findViewById(R.id.detailsTextView);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         // Persons Name
         String personName = (String) getIntent().getExtras().get("personName");
@@ -30,21 +34,27 @@ public class BrainDetailActivity extends AppCompatActivity {
         switch (personName){
             case "Nikola Tesla":
                 bornTextView.setText("Serbian American");
+                imageView.setImageResource(R.mipmap.nikola);
                 break;
             case "Elon Musk":
                 bornTextView.setText("South African");
+                imageView.setImageResource(R.mipmap.elon);
                 break;
             case "Bill Gates":
                 bornTextView.setText("U.S. Born");
+                imageView.setImageResource(R.mipmap.bill);
                 break;
             case "Steve Jobs":
                 bornTextView.setText("U.S. Born");
+                imageView.setImageResource(R.mipmap.steve);
                 break;
             case "Larry Page":
                 bornTextView.setText("U.S. Born");
+                imageView.setImageResource(R.mipmap.larry);
                 break;
             case "Sergey Brin":
                 bornTextView.setText("Soviet Born");
+                imageView.setImageResource(R.mipmap.sergey);
                 break;
             default:
                 break;
